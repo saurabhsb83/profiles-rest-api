06d6e30b98368d4557b300584ab1606f79fac1fd
Saurabh <saurabh.sb83@gmail.com>
2020-04-08 21:03:52 +0530

    Added search feature and permission.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from profiles_api import serializers
+from profiles_api import serializers , models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+from rest_framework import filters
 
 
 # Create your views here.
@@ -104,7 +107,14 @@ class HelloViewSet(viewsets.ViewSet):
 
 
 
-
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
 
 
